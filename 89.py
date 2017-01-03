c29524369104e3aaf9efd0ced023294cbcadbe2f
Joshua Ethridge <joshua.ethridge13@gmail.com>
2017-01-03 13:47:53 -0500

    Completed 89

--- 89.py
+++ 89.py
@@ -1,3 +1,16 @@
 # Problem: Take the 89.txt file and see if it can be improved, counting the total number of characters saved.
 
-#TODO
+import functions
+import time
+
+start = time.time()
+chars = 0
+f = open("89.txt", "r")
+for line in f:
+    l = line[:len(line)-1]
+    number = functions.numberFromRomanNumeral((l))
+    improved = functions.numberToRomanNumeral(number)
+    if len(str(l)) > len(improved):
+        chars += len(l) - len(improved)
+print("File can be improved by " + str(chars) + " characters.")
+functions.printTimeElapsed(start)
--- functions.py
+++ functions.py
@@ -22,6 +22,130 @@ def getIndividualDigits(n):
         n = int(math.floor(n))
     return digits[::-1]
 
+# This functions transforms a given number into a Roman numeral
+# It can accurately represent 1-3999 or any number if special characters (the characters with accents) are ignored.
+def numberToRomanNumeral(n):
+    # I = 1
+    # V = 5
+    # X = 10
+    # L = 50
+    # C = 100
+    # D = 500
+    # M = 1000
+    numeral = ""
+    thousands = n / 1000
+    thousands = int(thousands)
+    if thousands > 0:
+        for i in range(thousands):
+            numeral += "M"
+    n %= 1000
+    nineHundreds = n / 900
+    nineHundreds = int(nineHundreds)
+    if nineHundreds > 0:
+        numeral += "CM"
+    n %= 900
+    hundreds = n / 100
+    hundreds = int(hundreds)
+    if hundreds > 0:
+        if hundreds > 4:
+            numeral += "D"
+            hundreds -= 5
+        if hundreds == 4:
+            numeral += "CD"
+        else:
+            for i in range(hundreds):
+                numeral += "C"
+    n %= 100
+    nineties = n / 90
+    nineties = int(nineties)
+    if nineties > 0:
+        numeral += "XC"
+    n %= 90
+    tens = n / 10
+    tens = int(tens)
+    if tens > 0:
+        if tens > 4:
+            numeral += "L"
+            tens -= 5
+        if tens == 4:
+            numeral += "XL"
+        else:
+            for i in range(tens):
+                numeral += "X"
+    n %= 10
+    nines = n / 9
+    nines = int(nines)
+    if nines > 0:
+        numeral += "IX"
+    n %= 9
+    ones = n
+    if ones > 0:
+        if ones > 4:
+            numeral += "V"
+            ones -= 5
+        if ones == 4:
+            numeral += "IV"
+        else:
+            for i in range(ones):
+                numeral += "I"
+    return numeral
+
+
+# This function will translate from a Roman numeral to a numeric.
+# It functions perfectly with numberToRomanNumeral() for all cases tested, 1-1,000,000
+def numberFromRomanNumeral(n):
+    # I = 1
+    # V = 5
+    # X = 10
+    # L = 50
+    # C = 100
+    # D = 500
+    # M = 1000
+    number = 0
+    n = n.upper()
+    # Add a null terminator so the next character can easily be checked at the end of a string with no change in outcome
+    n += "0"
+    run = True
+    for i in range(len(n)):
+        if run:
+            if n[i] == "M":
+                number += 1000
+            elif n[i] == "C" and n[i+1] == "M":
+                number += 900
+                run = False
+            elif n[i] == "D":
+                number += 500
+            elif n[i] == "C" and n[i+1] == "D":
+                number += 400
+                run = False
+            elif n[i] == "C":
+                number += 100
+            elif n[i] == "X" and n[i+1] == "C":
+                number += 90
+                run = False
+            elif n[i] == "L":
+                number += 50
+            elif n[i] == "X" and n[i+1] == "L":
+                number += 40
+                run = False
+            elif n[i] == "X":
+                number += 10
+            elif n[i] == "I" and n[i+1] == "X":
+                number += 9
+                run = False
+            elif n[i] == "V":
+                number += 5
+            elif n[i] == "I" and n[i+1] == "V":
+                number += 4
+                run = False
+            elif n[i] == "I":
+                number += 1
+        else:
+            run = True
+    return number
+
+
+
 # This function will print the elapsed time given the start time
 def printTimeElapsed(start):
     end = time.time()
